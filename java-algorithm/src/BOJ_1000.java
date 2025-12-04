6ef7b5d102aeca2684ad49c1a8395da507cc62ba
김도현 <dojin8351@gmail.com>
2025-12-04 22:02:47 +0900

    init: project setup

---
+++ java-algorithm/src/BOJ_1000.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class BOJ_1000 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        System.out.println(a + b);
+    }
+}
